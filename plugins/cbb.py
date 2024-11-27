2effb9b27a55ecd07c8459c0d9e11f178e119bd6
Yash <titanmattersbot05@gmail.com>
2024-11-27 20:28:31 +0530

    TitanXBots

---
+++ plugins/cbb.py
@@ -0,0 +1,58 @@
+# Dont Remove Credit
+# Update Channel - TitanXBots
+# Ask Any Doubt on Telegram - @TitanOwner
+
+from pyrogram import Client
+from bot import Bot
+from config import *
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from database.database import add_user, del_user, full_userbase, present_user
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "help":
+        await query.message.edit_text(
+            text=HELP_TXT.format(first=query.from_user.first_name),
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                    InlineKeyboardButton("⚓ ʜᴏᴍᴇ", callback_data = "start"),
+                    InlineKeyboardButton("⚡ ᴄʟᴏꜱᴇ", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "about":
+        await query.message.edit_text(
+            text=ABOUT_TXT.format(first=query.from_user.first_name),
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                    InlineKeyboardButton("⚓ ʜᴏᴍᴇ", callback_data = "start"),
+                    InlineKeyboardButton("⚡ ᴄʟᴏꜱᴇ", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "start":
+        await query.message.edit_text(
+            text=START_MSG.format(first=query.from_user.first_name),
+            disable_web_page_preview=True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                    InlineKeyboardButton("🧠 ʜᴇʟᴘ", callback_data = "help"),
+                    InlineKeyboardButton("🔰 ᴀʙᴏᴜᴛ", callback_data = "about")
+                    ]
+                ]
+            )
+        )            
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
