9b38461d519bbd2a64e91c6d300d9cccb0723e93
Yash <titanmattersbot05@gmail.com>
2025-01-04 16:53:22 +0530

    TitanXBots

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7517200943:AAHd1Jm-cUD4P7qZGTQJSttzr2EeDZItMmQ")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "12293838"))
